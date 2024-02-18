a1a243c08ea46f85b61d9cb01f7e0517fe1a389c
indexedakki <74480055+indexedakki@users.noreply.github.com>
2024-02-18 23:26:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ import sys
 # Display the Python version
 st.write(f"Python version: {sys.version}")
 
-%pip list
+import subprocess
+import sys
+
+def pip_list():
+    args = [sys.executable, "-m", "pip", "list"]
+    p = subprocess.run(args, check=True, capture_output=True)
+    return p.stdout.decode()
+
+print(pip_list())
 
 # loading PDF, DOCX and TXT files as LangChain Documents
 def load_document(file):
